c7d3dc5843ca25eb646987a41c7d252a99117013
Minajeanne <albrown07@mac.com>
2019-05-14 18:24:53 -0400

    add completion? to stats

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_14_221809) do
+ActiveRecord::Schema.define(version: 2019_05_14_222344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2019_05_14_221809) do
     t.integer "race_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completion?"
   end
 
   create_table "user_prs", force: :cascade do |t|
