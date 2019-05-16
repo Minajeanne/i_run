36bce24ead9b4b185735ab4cfd56c027eeb78617
Minajeanne <albrown07@mac.com>
2019-05-16 10:30:12 -0400

    create user_pr new

--- app/controllers/user_prs_controller.rb
+++ app/controllers/user_prs_controller.rb
@@ -1,2 +1,29 @@
 class UserPrsController < ApplicationController
+
+  def index
+    @user = current_user
+    @prs = current_user.user_prs
+  end
+
+  def show
+    # show all users?
+    @user = current_user
+    @prs = current_user.user_prs
+  end
+
+  def new
+    @prs = current_user.user_prs.build
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
