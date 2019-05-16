ced9163f47dbd23d0def6236d0eb7788dec7a06e
Minajeanne <albrown07@mac.com>
2019-05-16 11:17:29 -0400

    fix pr form submit

--- app/controllers/user_prs_controller.rb
+++ app/controllers/user_prs_controller.rb
@@ -19,11 +19,23 @@ class UserPrsController < ApplicationController
   end
 
   def create
+    @pr = UserPr.new(user_pr_params)
+    if @pr.save
+      redirect_to user_prs_path(@pr), notice: "Your PR was saved!"
+    else
+      redirect_to new_user_pr_path, alert: "Your PR did not save. Please try again."
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def user_pr_params
+    params.require(:user_pr).permit(:name, :description)
   end
 end
