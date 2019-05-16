20e85fb85723b16f97ebd2b72374e687dbd5de31
Minajeanne <albrown07@mac.com>
2019-05-16 10:38:20 -0400

    add stats index

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,2 +1,29 @@
 class StatsController < ApplicationController
+
+  def index
+    @user = current_user
+    @stats = current_user.stats
+  end
+
+  def show
+    # show all users races?
+    @user = current_user
+    @stats = current_user.stats
+  end
+
+  def new
+    @stats = current_user.stats.build
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
