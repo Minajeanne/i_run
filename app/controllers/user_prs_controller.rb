54100f72003d27a0137cd6e5601462a0939f9427
Minajeanne <albrown07@mac.com>
2019-05-16 13:06:39 -0400

    add delete function to prs

--- app/controllers/user_prs_controller.rb
+++ app/controllers/user_prs_controller.rb
@@ -5,18 +5,18 @@ class UserPrsController < ApplicationController
     @prs = current_user.user_prs
   end
 
-  def show
-    # show all users?
-    @user = current_user
-    @prs = current_user.user_prs
-  end
+  # def show
+  #   # show all users?
+  #   @user = current_user
+  #   @prs = current_user.user_prs
+  # end
 
   def new
     @pr = current_user.user_prs.build
   end
 
   def edit
-    @prs = current_user.user_prs
+    @pr = current_user.user_prs.find_by(id: params[:id])
   end
 
   def create
@@ -29,21 +29,24 @@ class UserPrsController < ApplicationController
   end
 
   def update
+    @pr = current_user.user_prs.find_by(id: params[:id])
     if @pr.user_id == current_user.id
       @pr.update(user_pr_params)
       if @pr.save
-        redirect_to user_prs_path(@pr), notice: "Your PR was updated!"
+        redirect_to user_prs_path, notice: "Your PR was updated!"
       else
-        redirect_to edit_user_pr_path
+        redirect_to edit_user_pr_path, alert: "Your PR did not update. Please try again."
       end
     else
-      redirect_to user_prs_path(@pr), alert: "You are not authorized to edit this PR."
+      redirect_to user_prs_path, alert: "You are not authorized to edit this PR."
     end
   end
 
   def destroy
+    @pr = current_user.user_prs.find_by(id: params[:id])
     if @pr.user_id == current_user.id
-      redirect_to user_prs_path, notice: "Your PR was deleted"
+      @pr.destroy
+      redirect_to user_prs_path, notice: "Your PR was deleted."
     else
       redirect_to user_prs_path, notice: "Your are not authorized to delete this PR."
     end
