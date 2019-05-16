32d5a5379695152b50d764387ca194075c006146
Minajeanne <albrown07@mac.com>
2019-05-16 10:35:10 -0400

    create races new

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,2 +1,29 @@
 class RacesController < ApplicationController
+
+  def index
+    @user = current_user
+    @races = current_user.races
+  end
+
+  def show
+    # show all users races?
+    @user = current_user
+    @races = current_user.races
+  end
+
+  def new
+    @races = current_user.races.build
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
