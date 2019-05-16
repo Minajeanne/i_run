6c3c07612a35f3d47fc1f711893c48663e0b0b37
Minajeanne <albrown07@mac.com>
2019-05-16 18:21:15 -0400

    add location column to races

---
+++ db/migrate/20190516222021_add_location_to_races.rb
@@ -0,0 +1,5 @@
+class AddLocationToRaces < ActiveRecord::Migration[5.2]
+  def change
+    add_column :races, :location, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_16_170946) do
+ActiveRecord::Schema.define(version: 2019_05_16_222021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2019_05_16_170946) do
     t.datetime "updated_at", null: false
     t.date "event_date"
     t.string "distance"
+    t.string "location"
   end
 
   create_table "stats", force: :cascade do |t|
