fa282651e12f7dd1babc6bcc6ae70b24b7765ca4
Minajeanne <albrown07@mac.com>
2019-05-17 16:08:43 -0400

    remove has_many locations

--- app/models/race.rb
+++ app/models/race.rb
@@ -1,10 +1,15 @@
 class Race < ApplicationRecord
-  has_many :locations
   has_many :stats
   has_many :users, through: :stats
   accepts_nested_attributes_for :locations
 
   def current_stat(current_user)
     self.stats.where(user: current_user).first
+  end
+
+  def self.race_locations
+    self.reject { |l| l.to_s.empty? }
+    # self.select{|location| location != nil}
+    binding.pry
   end
 end
