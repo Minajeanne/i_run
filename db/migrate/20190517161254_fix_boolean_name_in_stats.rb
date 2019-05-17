42430d015f3f67fff2345ffc831941e5fe791871
Minajeanne <albrown07@mac.com>
2019-05-17 12:18:42 -0400

    rename stats completion to default false

--- db/migrate/20190517161254_fix_boolean_name_in_stats.rb
+++ db/migrate/20190517161254_fix_boolean_name_in_stats.rb
@@ -1,5 +1,6 @@
 class FixBooleanNameInStats < ActiveRecord::Migration[5.2]
   def change
-    rename_column :stats, :completion, :boolean, default: false
+    remove_column :stats, :boolean => false
+    add_column :stats, :completion, :boolean, default: false
   end
 end
