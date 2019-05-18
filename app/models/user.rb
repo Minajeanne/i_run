e472583164b279705aa0979c8b3d0847e0708d19
Minajeanne <albrown07@mac.com>
2019-05-18 09:08:11 -0400

    add callbacks_controller / omniauth devise modules

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
   has_many :user_prs
   has_many :stats
   has_many :races, through: :stats
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:github]
+
+  
 
   def race_complete
     self.races.includes(:stats).where('stats.completion = true')
