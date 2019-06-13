7a6fe69affc10866aab4c21c29d481978666b5d5
Minajeanne <albrown07@mac.com>
2019-06-13 11:24:30 -0400

    trying to add css

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'rack-cors'
 gem 'materialize-sass'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
+gem 'coffee-script-source'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/assets/javascripts/user_prs.js
+++ app/assets/javascripts/user_prs.js
@@ -24,7 +24,15 @@ function getPrs() {
     let mypr = new UserPr(data[0])
     let myPrHTML = mypr.prHTML()
 // complete **********
-    document.getElementById('new-pr-form').innerHTML += myPrHTML
+    document.getElementById('').innerHTML += myPrHTML
+  })
+}
+
+function listenForNewPrClick() {
+  $('button#new-pr').on('click', function (event) {
+    event.preventDefault()
+    let newPrForm = UserPr.newPrForm()
+    document.querySelector('div#new-pr-form').innerHTML = newPrForm
   })
 }
 
@@ -34,6 +42,17 @@ class UserPr {
     this.name = obj.name
     this.description = obj.description
   }
+
+  static newPrForm() {
+    return (`
+      <strong>Add a New PR</strong>
+      <form>
+        <input id='pr-name' type='text' name='name'></input><br>
+        <input type='text' name='description'></input><br>
+        <input type ='submit'/>
+      </form>
+    `)
+  };
 }
 
 UserPr.prototype.prHTML = function () {
@@ -45,12 +64,13 @@ UserPr.prototype.prHTML = function () {
   `)
 };
 
-UserPr.prototype.newPrForm = function () {
-  return (`
-    <strong>Add a New PR</strong>
-    <form>
-      <input id='pr-name' type='text' name='name'></input><br>
-      <input type='textarea' name='description'></input><br>
-      <input type ='submit'/>
-  `)
-};
+// UserPr.prototype.newPrForm = function () {
+//   return (`
+//     <strong>Add a New PR</strong>
+//     <form>
+//       <input id='pr-name' type='text' name='name'></input><br>
+//       <input type='textarea' name='description'></input><br>
+//       <input type ='submit'/>
+//     </form>
+//   `)
+// };
